d3e0fb10de72266b2b74faa2c3c9bddd6c612d36
unknown <dishendramishra@gmail.com>
2020-09-09 14:00:58 +0530

    added oled example

---
+++ oled-example/boot.py
@@ -0,0 +1,28 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+#import esp
+#esp.osdebug(None)
+import uos, machine
+#uos.dupterm(None, 1) # disable REPL on UART(0)
+import gc
+#import webrepl
+#webrepl.start()
+gc.collect()
+
+
+import network
+
+#disabling access point interface
+ap_if = network.WLAN(network.AP_IF)
+ap_if.active(False)
+
+def do_connect():
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect('<essid>', '<password>')
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+do_connect()
